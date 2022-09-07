a70b0d42436295ad14772a044c77ba5753000e87
jasmingko <77540071+jasmingko@users.noreply.github.com>
2022-09-07 10:43:28 +0200

    search page /controller update etc

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -9,7 +9,7 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="/">E-Comm</a>
+        <a class="navbar-brand" href="/">ElctroShop</a>
       </div>
   
       <!-- Collect the nav links, forms, and other content for toggling -->

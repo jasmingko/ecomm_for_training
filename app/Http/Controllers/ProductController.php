8d36c50e426bca0b7da38bbbd02d9ef093edb126
jasmingko <77540071+jasmingko@users.noreply.github.com>
2022-09-07 12:42:58 +0200

    creating cart db

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
 class ProductController extends Controller
 {
     function index()
@@ -11,14 +12,29 @@ class ProductController extends Controller
         $data= Product::all();
         return view('product',['products'=>$data]);
     }
+    
     function detail($id)
     {
         
         $data= Product::find($id);
         return view('detail',['product'=>$data]);
     }
+    
     function search(Request $req)
     {
        $data= Product::where('name','like','%'.$req->input('query').'%')->get();
      return view('search',['products'=>$data]);}
+    
+     function addToCart(Request $req)
+    {
+        if($req->session()->has('user'))
+        {   
+            $cart=new Cart;
+            $cart->user_id=$req->session()->get('user')['id'];
+            $cart->product_id=$req->product_id;
+            $cart->save();
+            return redirect('/');
+        }
+        else{return redirect('/login');}
+    }
 }
